cbe987f338deb142f46561ed6cc1d47dca607633
Savely Rosenaur <71658837+zangezi39@users.noreply.github.com>
2022-12-02 20:44:20 +0100

    Add files via upload

---
+++ arbitrage/config/symbol_excluded.js
@@ -0,0 +1,5 @@
+var symbol_excluded = [];
+symbol_excluded.push('STEEM/BTC');  // sample excluded symbol
+symbol_excluded.push('STEEM/ETH');  // sample excluded symbol
+
+module.exports = symbol_excluded;
